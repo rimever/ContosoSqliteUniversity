d0fcc87eee5fa598a0f801f6f25af87a8d794016
rimever <mot_siva_ordin@hotmail.com>
2019-09-21 23:05:47 +0900

    関連データの読み取りまで実装。

--- ContosoUniversity/Models/Courses.cs
+++ ContosoUniversity/Models/Courses.cs
@@ -1,7 +1,10 @@
 ﻿#region
 
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
 
 #endregion
 
@@ -10,11 +13,39 @@ namespace ContosoUniversity.Models
     public class Course
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
         public int CourseID { get; set; }
 
+        [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+        [Range(0,5)]
         public int Credits { get; set; }
 
+        public int DepartmentID { get; set; }
+        public Department Department { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+    }
+
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        [StringLength(50,MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Budget { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        public int? InstructorID { get; set; }
+
+        public Instructor Administrator { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
     }
 }
