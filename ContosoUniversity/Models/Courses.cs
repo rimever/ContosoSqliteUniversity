96b5f60bd675495d78f316b9689e6d5bc63302fd
rimever <mot_siva_ordin@hotmail.com>
2019-09-22 16:29:24 +0900

    コンカレンシーの競合の処理まで実装。

--- ContosoUniversity/Models/Courses.cs
+++ ContosoUniversity/Models/Courses.cs
@@ -1,6 +1,5 @@
 ﻿#region
 
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -25,27 +24,5 @@ namespace ContosoUniversity.Models
         public Department Department { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
         public ICollection<CourseAssignment> CourseAssignments { get; set; }
-    }
-
-    public class Department
-    {
-        public int DepartmentID { get; set; }
-        [StringLength(50,MinimumLength = 3)]
-        public string Name { get; set; }
-
-        [DataType(DataType.Currency)]
-        [Column(TypeName = "money")]
-        public decimal Budget { get; set; }
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Start Date")]
-        public DateTime StartDate { get; set; }
-
-        public int? InstructorID { get; set; }
-
-        public Instructor Administrator { get; set; }
-
-        public ICollection<Course> Courses { get; set; }
     }
 }
